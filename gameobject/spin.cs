702c2b070aae7a53721052ecb449889650451c29
massimilianoC <massimiliano.camillucci@gmail.com>
2020-06-16 15:23:49 +0200

    Create spin.cs

---
+++ gameobject/spin.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    public Vector3 current;
+    public Vector3 destination;
+    public bool spinX, spinY, spinZ = false;
+    public float spinValX, spinValY, spinValZ = 0.0f;
+    public Vector3 spinSpeed = new Vector3(0.1f, 0.1f, 0.1f);
+    public float time;
+    public Vector3 currentVelocity = new Vector3(0,0,0);
+    public float smooth = 0.5f;
+    public Quaternion rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.deltaTime;
+    }
+
+       void Update()
+    {
+        tiggerSpinAxis();
+        current = this.gameObject.transform.rotation.eulerAngles;
+        destination = new Vector3(current.x+ spinValX, current.y+ spinValY, current.z+ spinValZ);
+        rotation.eulerAngles = Vector3.SmoothDamp(current, destination, ref currentVelocity, time * smooth); ;
+        this.gameObject.transform.rotation = rotation;
+    }
+
+    void tiggerSpinAxis()
+    {
+        //X Axys
+        if (spinX)
+        {
+            spinValX= spinSpeed.x;
+        }
+        else
+        {
+            spinValX = 0;
+        }
+
+        //Y Axys
+        if (spinY)
+        {
+            spinValY = spinSpeed.y;
+        }
+        else
+        {
+            spinValY = 0;
+        }
+
+        //Z Axys
+        if (spinZ)
+        {
+            spinValZ = spinSpeed.z;
+        }
+        else
+        {
+            spinValZ = 0;
+        }
+    }
+
+    public void toggleSpinX()
+    {
+        if (spinX)
+        {
+            spinX = false;
+        } else
+        {
+            spinX = true;
+        }
+    }
+
+    public void toggleSpinY()
+    {
+        if (spinX)
+        {
+            spinY = false;
+        }
+        else
+        {
+            spinY = true;
+        }
+    }
+
+    public void toggleSpinZ()
+    {
+        if (spinX)
+        {
+            spinZ = false;
+        }
+        else
+        {
+            spinZ = true;
+        }
+    }
+}
